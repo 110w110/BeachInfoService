bd3802ab488ebeb4e5f3774beb23c3146d930666
110w110 <110w110.official@gmail.com>
2023-07-24 16:04:52 +0900

    프로젝트 계층 정리

---
+++ BeachInfoService/Model/Beach.h
@@ -0,0 +1,16 @@
+//
+//  Beach.h
+//  BeachInfoService
+//
+//  Created by 한태희 on 2023/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Beach : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BeachInfoService/Model/Beach.m
@@ -0,0 +1,12 @@
+//
+//  Beach.m
+//  BeachInfoService
+//
+//  Created by 한태희 on 2023/07/24.
+//
+
+#import "Beach.h"
+
+@implementation Beach
+
+@end
